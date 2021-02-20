0a6d361687f3a4047f6731b3541c0910dbf070bd
Graham Campbell <graham@alt-three.com>
2021-02-20 20:50:21 +0000

    Release 3.0.0

--- app/Console/Application.php
+++ app/Console/Application.php
@@ -30,7 +30,7 @@ final class Application extends SymfonyApplication
      *
      * @var string
      */
-    const APP_VERSION = '2.0.9';
+    const APP_VERSION = '3.0.0';
 
     /**
      * Create a new StyleCI CLI application.
